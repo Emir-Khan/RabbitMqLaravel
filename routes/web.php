34d9c80651f445f2f2a0270f49df3001ec0d1537
customoyuncu <kraloyuntv2@gmail.com>
2022-01-19 16:48:22 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name("mainPage");
 
 
-Route::view('/login', 'login');
+Route::view('/login', 'login')->name("login");
 Route::post("/login/post","RequestController@postLogin");
 
-Route::view('/register', 'register');
+Route::view('/register', 'register')->name("register");
 Route::post("/register/post","RequestController@postRegister");
 
 Route::get('/forgot/password', "RequestController@forgotPassword")->name("resetPassword");
-Route::post("/forgot/password/reset","RequestController@postForgotPassword")->name("postResetPassword");
+Route::post("/forgot/password/reset","RequestController@postForgotPassword");
